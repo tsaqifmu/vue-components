d11bce5543f585975549986f42cf2c47f3fcda43
Ochaa <mushoddaqt@gmail.com>
2025-09-27 17:23:39 +0700

    Refactor eslint.config.ts imports for improved organization
    - Reordered and grouped import statements in eslint.config.ts for better readability.
    - Moved Storybook and Vue-related imports to follow a consistent structure.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,10 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
 
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import { globalIgnores } from 'eslint/config'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
